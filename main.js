56507994d19f8333ac7336daada97687bbbee21c
adel-alshoufi <alshoufiadel@gmail.com>
2026-03-16 09:36:38 +0200

    menu

--- main.js
+++ main.js
@@ -2,25 +2,20 @@ const $ = selector => document.querySelector(selector);
 const $$ = selector => document.querySelectorAll(selector);
 
 const menuButton = $("#menu");
+const closeButton = $("#close-menu");
 const mobileNav = $(".mobile-nav");
 const mobileNavLinks = $$(".mobile-nav li");
 
-let open = false;
 menuButton.addEventListener("click", () => {
-    if(!open) {
-        mobileNav.style.display = "flex";
-        open = true
-    } else {
-        mobileNav.style.display = "none";
-        open = false
-    }
-})
+    mobileNav.classList.add("nav-open");
+});
+
+closeButton.addEventListener("click", () => {
+    mobileNav.classList.remove("nav-open");
+});
 
 mobileNavLinks.forEach(link => {
     link.addEventListener("click", () => {
-        mobileNav.style.display = "none";
-        open = false
-    })
-})
-
-console.log(mobileNavLinks)
+        mobileNav.classList.remove("nav-open");
+    });
+});
